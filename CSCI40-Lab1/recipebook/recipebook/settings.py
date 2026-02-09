c23d9d0fc32d7c2e58d7ec5aa97bdfdf65b82ec7
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-02-09 11:13:19 +0800

    First Attempt - Failed
    Attempted to implement an app. Failed attempt.

--- CSCI40-Lab1/recipebook/recipebook/settings.py
+++ CSCI40-Lab1/recipebook/recipebook/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ledger'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'recipebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
