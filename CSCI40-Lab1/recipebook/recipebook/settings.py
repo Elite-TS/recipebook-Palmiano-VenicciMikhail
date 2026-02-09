3afc21f2c10034a20d68e47976097fea25ac6d04
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-02-09 08:58:49 +0800

    Working Project
    Made the working base project.

--- CSCI40-Lab1/recipebook/recipebook/settings.py
+++ CSCI40-Lab1/recipebook/recipebook/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--7$=9qi%de5%149)e1l-_096q#*d+)%=@rz&w0he#34%+n_=q&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
