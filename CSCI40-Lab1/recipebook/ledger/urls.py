700fd89eaa0588ff6ad32259efe72b970f2b8fce
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-02-09 15:39:45 +0800

    Implemented Page: Recipe List
    Implemented the Recipe List page with working links

--- CSCI40-Lab1/recipebook/ledger/urls.py
+++ CSCI40-Lab1/recipebook/ledger/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import recipe1
+from .views import recipe1, recipe2, recipes_list
 
 app_name = "ledger"
 
 urlpatterns = [
     path('recipe/1', recipe1, name='recipe1'),
+    path('recipe/2', recipe2, name='recipe2'),
+    path('recipes/list', recipes_list, name='recipes_list')
 ]
--- CSCI40-Lab1/recipebook/ledger/views.py
+++ CSCI40-Lab1/recipebook/ledger/views.py
@@ -29,5 +29,109 @@ def recipe1(request):
         ],
         "link": "/recipe/1"
     }
-
     return render(request, "show_recipe.html", ctx)
+
+def recipe2(request):
+    ctx = {
+        "name": "Recipe 2",
+        "ingredients": [
+            {
+                "name": "garlic",
+                "quantity": "1 head"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "vinegar",
+                "quantity": "1/2cup"
+            },
+            {
+                "name": "water",
+                "quantity": "1 cup"
+            },
+            {
+                "name": "salt",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "whole black peppers",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "pork",
+                "quantity": "1 kilo"
+            }
+        ],
+        "link": "/recipe/2"
+    }
+    return render(request, "show_recipe.html", ctx)
+
+def recipes_list(request):
+    ctx = {
+        "recipes": [
+            {
+                "name": "Recipe 1",
+                "ingredients": [
+                    {
+                        "name": "tomato",
+                        "quantity": "3pcs"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1kg"
+                    },
+                    {
+                        "name": "water",
+                        "quantity": "1L"
+                    },
+                    {
+                        "name": "sinigang mix",
+                        "quantity": "1 packet"
+                    }
+                ],
+                "link": "/ledger/recipe/1"
+            },
+            {
+                "name": "Recipe 2",
+                "ingredients": [
+                    {
+                        "name": "garlic",
+                        "quantity": "1 head"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    { 
+                        "name": "vinegar",
+                        "quantity": "1/2cup"
+                    },
+                    {
+                        "name": "water",
+                        "quanity": "1 cup"
+                    },
+                    {
+                        "name": "salt",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "whole black peppers",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1 kilo"
+                    }
+                ],
+                "link": "/ledger/recipe/2"
+            }
+        ]
+    }
+    return render(request, "list.html", ctx)
+
