0a3bbac26c8ffaa2a8d491144aa3046c937f045f
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-02-19 17:52:11 +0800

    Code Clean Up
    Removing extra code comments.

--- CSCI40-Lab1/recipebook/ledger/urls.py
+++ CSCI40-Lab1/recipebook/ledger/urls.py
@@ -5,9 +5,6 @@ from .views import RecipeList, ShowRecipe
 app_name = "ledger"
 
 urlpatterns = [
-    # path('recipe/1', recipe1, name='recipe1'),
-    # path('recipe/2', recipe2, name='recipe2'),
-    # path('recipes/list', recipes_list, name='recipes_list')
     path('recipes/list', RecipeList.as_view(), name='recipes_list'),
     path('<int:pk>/', ShowRecipe.as_view(), name='recipe_detail'),
 ]
