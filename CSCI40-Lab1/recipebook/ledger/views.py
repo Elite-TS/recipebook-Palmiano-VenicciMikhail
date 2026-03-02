4988ec7e18a055fc5fa1cca4e3f26340282f8376
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-03-02 10:12:08 +0800

    Implemented Required Login
    Implemented the required login for viewing recipes. A test user and test recipe were used to test if it worked.

--- CSCI40-Lab1/recipebook/ledger/views.py
+++ CSCI40-Lab1/recipebook/ledger/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Recipe
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -9,6 +10,6 @@ class RecipeList(ListView):
     model = Recipe
     template_name = 'list.html'
 
-class ShowRecipe(DetailView):
+class ShowRecipe(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'show_recipe.html'
--- CSCI40-Lab1/recipebook/useraccounts/models.py
+++ CSCI40-Lab1/recipebook/useraccounts/models.py
@@ -7,3 +7,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     short_bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.name)
