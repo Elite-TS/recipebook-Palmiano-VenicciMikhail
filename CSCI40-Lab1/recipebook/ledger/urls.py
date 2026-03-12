f69b2b99fe98b81e4e3d85767c3c76526f0b9849
Elite-TS <venicci.mikhail.palmiano@student.ateneo.edu>
2026-03-12 23:01:39 +0800

    Implemented User Recipe Creation Pages
    Implemented the pages for users to create their own recipes. This includes the page for uploading images, and the bonus point.

--- CSCI40-Lab1/recipebook/ledger/urls.py
+++ CSCI40-Lab1/recipebook/ledger/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import RecipeList, ShowRecipe
+from .views import RecipeList, ShowRecipe, RecipeImageUploadView, RecipeCreateView
 
 app_name = "ledger"
 
 urlpatterns = [
     path('recipes/list', RecipeList.as_view(), name='recipes_list'),
     path('recipe/<int:pk>/', ShowRecipe.as_view(), name='recipe_detail'),
+    path('recipe/add', RecipeCreateView.as_view(), name='recipe_create'),
+    path(
+        'recipe/<int:pk>/add_image',
+        RecipeImageUploadView.as_view(),
+        name='recipe_image_upload',
+    ),
 ]
--- CSCI40-Lab1/recipebook/ledger/views.py
+++ CSCI40-Lab1/recipebook/ledger/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import Recipe
+from .models import Recipe, RecipeImage
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from .forms import RecipeForm, RecipeImageForm
 
 # Create your views here.
 
@@ -15,3 +19,32 @@ class RecipeList(ListView):
 class ShowRecipe(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'show_recipe.html'
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'create_recipe.html'
+
+class RecipeImageUploadView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = 'image_upload_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipe'] = get_object_or_404(Recipe, pk=self.kwargs['pk'])
+        return context
+    
+    def get_success_url(self):
+        return reverse_lazy(
+            'ledger:recipe_detail',
+            kwargs={'pk': self.kwargs['pk']}
+        )
+    
+    def post(self, request, *args, **kwargs):
+        form = RecipeImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.instance.recipe_id = self.kwargs['pk']
+            form.save()
+        return redirect(self.get_success_url())
+
